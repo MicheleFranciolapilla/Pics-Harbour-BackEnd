0d0958137c05f51fe1508ced10b0db018a39c1e1
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2024-01-23 10:50:21 +0100

    Package.json fixed

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // Importazione librerie
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 
 // Implementazione server
 const   port = process.env.port
 const   server = express();
+        // Settaggio CORS Policy
+        server.use(cors(
+                {
+                        origin          :       "*",
+                        methods         :       "GET, POST, PUT, DELETE",
+                        credentials     :       true
+                }));
         server.listen(port, () => console.log(`Server in esecuzione su ${process.env.HOST}${port}`));
