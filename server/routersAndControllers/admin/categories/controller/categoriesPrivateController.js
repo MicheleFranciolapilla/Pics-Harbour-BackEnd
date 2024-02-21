917a0c81092f6caeef6b6398da87e058f4a7dbe6
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2024-02-21 09:48:06 +0100

    Categories (private), method Update cleaned and commented

--- server/routersAndControllers/admin/categories/controller/categoriesPrivateController.js
+++ server/routersAndControllers/admin/categories/controller/categoriesPrivateController.js
@@ -97,93 +97,83 @@ async function update(req, res, next)
         errorToThrow = new ErrorResourceNotFound(`Category Id [${id}]`, "CATEGORIES (private) - UPDATE");
     else
     {
-        // A rigor di logica, se il middleware autorizzativo ha permesso di giungere fin quì significa che il richiedente è un Super Admin.
-        // Ad ogni modo si effettua il controllo specifico, seppur ridondante
-        if (req.tokenOwner.role !== "Super Admin")
-            errorToThrow = new ErrorUserNotAllowed("User not allowed to modify categories", "CATEGORIES (PRIVATE) - UPDATE");
-        else
+        // Se il middleware autorizzativo ha permesso di giungere fin quì significa che il richiedente è un Super Admin, quindi non si effettua il check in tal senso.
+        const nameSlug = (name) ? basicSlug(name) : categoryIdCheck.data.slug;
+        // Si setta l'attributo "data" della "prismaQuery" con tutti i dati corretti, al fine di poter effettuare tutte le seguenti chiamate al DB
+        // Il settaggio è tale che, nella query figurino tutti i dati aggiornati della category, pronti per l'update.
+        // Vengono inoltre rimossi i campi che ostacolerebbero il corretto funzionamento del metodo "update"
+        prismaQuery["data"] = {...categoryIdCheck.data};
+        prismaQuery.data.userId = req.tokenOwner.id;
+        if (name)
+            prismaQuery.data.name = name;
+        prismaQuery.data.slug = nameSlug;
+        removeProperties([prismaQuery.data], "id", "createdAt", "updatedAt", "pictures");
+        // Il flag "canProceed" consente di unificare i percorsi relativi all'ipotetico "else" sulla seguente condizione e al caso di "slug" valido dentro l'"if"
+        let canProceed = true;
+        if ((name) && (nameSlug !== categoryIdCheck.data.slug))
+        // Se "name" è definito ed il nuovo "slug" è diverso dal vecchio, si verifica che non sia già presente nel db
         {
-            const nameSlug = (name) ? basicSlug(name) : categoryIdCheck.data.slug;
-            prismaQuery["data"] = {...categoryIdCheck.data};
-            prismaQuery.data.userId = req.tokenOwner.id;
-            if (name)
-                prismaQuery.data.name = name;
-            prismaQuery.data.slug = nameSlug;
-            removeProperties([prismaQuery.data], "id", "createdAt", "updatedAt", "pictures");
-            // Il flag "canProceed" consente di unificare i percorsi relativi all'ipotetico "else" sulla seguente condizione e al caso di "slug" valido dentro l'"if"
-            let canProceed = true;
-            if ((name) && (nameSlug !== categoryIdCheck.data.slug))
+            canProceed = false;
+            const categorySlugCheck = await prismaOperator(prisma, "category", "findUnique", { "where" : { "slug" : nameSlug } })
+            if (!categorySlugCheck.success)
+                errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "CATEGORIES (private) - UPDATE");
+            else if (categorySlugCheck.data)
+                errorToThrow = new ErrorRepeatedData("slug", "CATEGORIES (private) - UPDATE");
+            else    
+                canProceed = true;
+        }
+        if (canProceed)
+        // Giunti a questo punto, si aprono i seguenti scenari.....
+        // -A-  Non è stato richiesto l'upload di un nuovo file immagine, quindi, se un update ci sarà riguarderà il "name"
+        // -A1- "name" è definito --> si procede con l'update.
+        //      In caso di insuccesso si genera un errore
+        //      In caso di successo si rimanda al check finale di "disconnect" 
+        // -A2- "name" è undefined --> si rimanda al check finale di "disconnect"
+        // -B-  E' stato richiesto l'upload di un nuovo file immagine, quindi l'update riguarderà sicuramente il file e, eventualmente anche il "name"
+        // -B1- Il file non è valido --> si genera un errore
+        // -B2- Il file è valido --> si procede con l'update.
+        //      In caso di insuccesso si genera un errore
+        //      In caso di successo si rimanda al check finale di "disconnect"
+        // -C-  Si procede con il check finale di "disconnect" solo in assenza di errori (errorToThrow = null)
+        {
+            if (!fileData)
+            // Caso A
             {
-                // Se "name" è definito ed il nuovo "slug" è diverso dal vecchio, si verifica che non sia già presente nel db
-                canProceed = false;
-                const categorySlugCheck = await prismaOperator(prisma, "category", "findUnique", { "where" : { "slug" : nameSlug } })
-                if (!categorySlugCheck.success)
-                    errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "CATEGORIES (private) - UPDATE");
-                else if (categorySlugCheck.data)
-                    errorToThrow = new ErrorRepeatedData("slug", "CATEGORIES (private) - UPDATE");
-                else    
-                    canProceed = true;
-            }
-            if (canProceed)
-            {
-                // Giunti a questo punto, si aprono i seguenti scenari.....
-                // -A-  Non è stato richiesto l'upload di un nuovo file immagine, quindi, se un update ci sarà riguarderà il "name"
-                // -A1- "name" è definito --> si procede con l'update.
-                //      In caso di insuccesso si genera un errore
-                //      In caso di successo si rimanda al check finale di "disconnect" 
-                // -A2- "name" è undefined --> si rimanda al check finale di "disconnect"
-                // -B-  E' stato richiesto l'upload di un nuovo file immagine, quindi l'update riguarderà sicuramente il file e, eventualmente anche il "name"
-                // -B1- Il file non è valido --> si genera un errore
-                // -B2- Il file è valido --> si procede con l'update.
-                //      In caso di insuccesso si genera un errore
-                //      In caso di successo si rimanda al check finale di "disconnect"
-                // -C-  Si procede con il check finale di "disconnect" solo in assenza di errori (errorToThrow = null)
-
-                if (!fileData)
-                // Caso A
+                if (name)
+                // Caso A1
                 {
-                    if (name)
-                    // Caso A1
+                    const categoryToUpdate = await prismaOperator(prisma, "category", "update", prismaQuery);
+                    if (!(categoryToUpdate.success && categoryToUpdate.data))
+                        errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "CATEGORIES (private) - UPDATE");
+                    else
                     {
-                        // "dataQuery" è completa, si può quindi comporre la "prismaQuery" e procedere
-                        // prismaQuery = addPropertyAtPosition(prismaQuery, "data", dataQuery.data, 1);
-                        // prismaQuery["data"] = dataQuery.data;
-                        // console.log(prismaQuery)
-                        const categoryToUpdate = await prismaOperator(prisma, "category", "update", prismaQuery);
-                        if (!(categoryToUpdate.success && categoryToUpdate.data))
-                            errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "CATEGORIES (private) - UPDATE");
-                        else
-                        {
-                            responseObj["previous"] = {...categoryIdCheck.data};
-                            responseObj["updated"] = {...categoryToUpdate.data};
-                        }
+                        responseObj["previous"] = {...categoryIdCheck.data};
+                        responseObj["updated"] = {...categoryToUpdate.data};
                     }
                 }
+            }
+            else
+            // Caso B
+            {
+                uploadReport = fileUploadReport(req);
+                if (!uploadReport.File_uploaded)
+                // Caso B1 (file non valido; mai caricato oppure già cancellato dal multer middleware terminator)
+                    errorToThrow = new ErrorUnsupportedFile("Unsupported file: " + JSON.stringify(uploadReport, null, 3), "CATEGORIES (private) - UPDATE");
                 else
-                // Caso B
+                // Caso B2
                 {
-                    uploadReport = fileUploadReport(req);
-                    if (!uploadReport.File_uploaded)
-                    // Caso B1 (file non valido; mai caricato oppure già cancellato dal multer middleware terminator)
-                        errorToThrow = new ErrorUnsupportedFile("Unsupported file: " + JSON.stringify(uploadReport, null, 3), "CATEGORIES (private) - UPDATE");
+                    // si aggiunge la proprietà "thumb" a "prismaQuery.data" di modo da poter procedere con l'update con il nuovo file immagine
+                    // Essendo stato caricato il file, da multer, esiste sicuramente l'oggetto "file" nella request, contenente il nome del file
+                    prismaQuery.data.thumb = req.file.filename;
+                    const categoryToUpdate = await prismaOperator(prisma, "category", "update", prismaQuery);
+                    if (!(categoryToUpdate.success && categoryToUpdate.data))
+                        errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "CATEGORIES (private) - UPDATE");
                     else
-                    // Caso B2
                     {
-                        // si aggiunge la proprietà "thumb" a "dataQuery.data", dopodichè si può comporre la "prismaQuery" e procedere
-                        prismaQuery.data.thumb = req.file.filename;
-                        // prismaQuery = addPropertyAtPosition(prismaQuery, "data", dataQuery.data, 1);
-                        // prismaQuery["data"] = dataQuery.data;
-                        console.log(prismaQuery)
-                        const categoryToUpdate = await prismaOperator(prisma, "category", "update", prismaQuery);
-                        if (!(categoryToUpdate.success && categoryToUpdate.data))
-                            errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "CATEGORIES (private) - UPDATE");
-                        else
-                        {
-                            // In caso di successo bisognerà rimuovere dal server il vecchio file immagine
-                            deleteFileBeforeThrow(buildFileObject(categoryIdCheck.data.thumb), "CATEGORIES (private) - UPDATE (SUCCESSED)");
-                            responseObj["previous"] = {...categoryIdCheck.data};
-                            responseObj["updated"] = {...categoryToUpdate.data};
-                        }
+                        // In caso di successo bisognerà rimuovere dal server il vecchio file immagine
+                        deleteFileBeforeThrow(buildFileObject(categoryIdCheck.data.thumb), "CATEGORIES (private) - UPDATE (SUCCESSED)");
+                        responseObj["previous"] = {...categoryIdCheck.data};
+                        responseObj["updated"] = {...categoryToUpdate.data};
                     }
                 }
             }
@@ -202,13 +192,11 @@ async function update(req, res, next)
     }
     // Altrimenti si entra nel caso C, dentro cui ricade uno dei casi A1 (prosecuzione), A2, B2 (prosecuzione).
     // Nel caso C si valuta se è stata richiesta la disconnessione delle pictures dalla category in oggetto
-    console.log("DISCONNECT VALUE: ", disconnect);
     if (disconnect)
+    // Solo se "disconnect" è truthy (definito, non nullo e true) si procede alla disconnessione delle pictures
     {
-        console.log("MANAGING DISCONNECT");
         // Si completa la proprietà "data" della query, essendo "where" e "include" già definite e valide
         prismaQuery.data["pictures"] = { "set" : [] };
-        console.log(prismaQuery);
         const disconnectPictures = await prismaOperator(prisma, "category", "update", prismaQuery);
         if (!(disconnectPictures.success && disconnectPictures.data))
         {
@@ -222,12 +210,14 @@ async function update(req, res, next)
         else
         {
             // In caso di successo si aggiorna, in responseObj il campo "updated" e si comunica l'avvenuta disconnessione
+            // Si assegna anche il campo "previous" poichè potrebbe essere stata richiesta la sola disconnessione, senza alcun "update" di "name" o file
             responseObj["previous"] = {...categoryIdCheck.data};
             responseObj["updated"] = {...disconnectPictures.data};
             responseObj["disconnection"] = "success";
         }
     }
     // Si chiude restituendo lo status 200 con i dati riportati in "responseObj"
+    // Laddove non fosse stata eseguita alcuna operazione (name undefined, file immagine non caricato e disconnect undefined o false) si otterrà comunque uno status 200 con campi vuoti
     formattedOutput("CATEGORIES (private) - UPDATE - SUCCESS", "***** Status: 200", "***** Previous: ", responseObj.previous, "***** Updated: ", responseObj.updated, disconnect && "***** Pictures disconnection: ", disconnect && responseObj.disconnection);
     return res.json({...responseObj});
 }
