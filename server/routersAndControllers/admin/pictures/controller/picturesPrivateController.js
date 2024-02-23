a841630eb03d1d8483d219d8cf8c93c2119b2e46
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2024-02-23 08:53:27 +0100

    Refactoring pictures/update:OK

--- server/routersAndControllers/admin/pictures/controller/picturesPrivateController.js
+++ server/routersAndControllers/admin/pictures/controller/picturesPrivateController.js
@@ -7,7 +7,7 @@ const ErrorResourceNotFound = require("../../../../exceptionsAndMiddlewares/exce
 const ErrorUserNotAllowed = require("../../../../exceptionsAndMiddlewares/exceptions/ErrorUserNotAllowed");
 const ErrorRequestValidation = require("../../../../exceptionsAndMiddlewares/exceptions/ErrorRequestValidation");
 
-const { findAllCategories, createRecord, checkPictureOwnership, deletePictureIfOwner } = require("../../../../utilities/prismaCalls");
+const { findAllCategories, createRecord, checkPictureOwnership, updateRecord, deletePictureIfOwner } = require("../../../../utilities/prismaCalls");
 const { prismaOperator } = require("../../../../utilities/general");
 const { deleteFileBeforeThrow, buildFileObject } = require("../../../../utilities/fileManagement");
 const { formattedOutput } = require("../../../../utilities/consoleOutput");
@@ -35,8 +35,8 @@ async function store(req, res, next)
                                 "userId"        :   req.tokenOwner.id,
                                 "categories"    :   { "connect" :   connectedCategories.map( catId => ({ "id" : catId })) }
                             },
-            "include"   :   {   "categories"    :   { "select"  :   { "id" : true }}}
-        }
+            "include"   :   {   "categories"    :   { "select"  :   { "id" : true } } }
+        };
         const picture = await createRecord("picture", prismaQuery, "PICTURES (private) - STORE");
         formattedOutput("PICTURES (private) - STORE - SUCCESS", "***** Status: 201", "***** New Picture: ", picture);
         return res.status(201).json({ picture });
@@ -55,82 +55,40 @@ async function update(req, res, next)
     // - sono modificabili tutti i campi "dato", incluse le categories di riferimento
     // - è modificabile anche il titolo a patto che esso mantenga una lunghezza compresa tra il minimo e massimo consentito
     // - ovviamente non è modificabile la userId (utente titolare della foto)
-    let { id, title, description, visible, categories } = matchedData(req, { onlyValidData : true });
-    // Si inizia verificando che l'id della picture esista effettivamente nel database, 
-    // dopodichè, in caso affermativo, si verifica che lo user titolare della foto coincida con lo user che ne sta richiedendo la modifica
-    let errorToThrow = null;
-    let prismaQuery = { "where" : { "id" : id }, "include" : { "categories" : { "select" : { "id" : true } } } };
-    const pictureToUpdate = await prismaOperator(prisma, "picture", "findUnique", prismaQuery);
-    console.log("LOG: ",pictureToUpdate);
-    if (!pictureToUpdate.success)
-        errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "PICTURES (private) - UPDATE");
-    else if (!pictureToUpdate.data)
-        errorToThrow = new ErrorResourceNotFound(`Picture Id [${id}]`, "PICTURES (private) - UPDATE");
-    else
+    const { id, title, description, visible, categories } = matchedData(req, { onlyValidData : true });
+    try
     {
-        if (pictureToUpdate.data.userId !== req.tokenOwner.id)
-            errorToThrow = new ErrorUserNotAllowed("User not allowed to modify another user's picture", "PICTURES (private) - UPDATE");
-        else
+        // Si inizia eseguendo la funzione "checkPictureOwnership", la quale eseguirà i seguenti tasks:
+        // verificherà l'esistenza della picture, lanciando un errore in caso negativo, dopodichè
+        // verificherà che il richiedente la modifica sia il titolare della picture, fornendo il risultato booleano del check, più il record della picture trovata
+        const { ownership, picture } = await checkPictureOwnership(id, req.tokenOwner.id, "PICTURES (private) - UPDATE");
+        if (!ownership)
+            throw new ErrorUserNotAllowed("User not allowed to modify another user's picture", "PICTURES (private) - UPDATE");
+        const connectedCategories = categories ?? [];
+        const { missingCategories } = await findAllCategories(connectedCategories, "PICTURES (private) - UPDATE");
+        if (missingCategories.length !== 0)
+            throw new ErrorResourceNotFound(`Category Ids [${missingCategories}]`, "PICTURES (private) - UPDATE");
+        const prismaQuery =
         {
-            categories = categories ?? [];
-            let categoriesCheck = { "success" : true, "data" : [] };
-            let missingCategories = [];
-            if (categories.length !== 0)
-            {
-                // Si verifica che tutte le categories collegate siano effettivamente esistenti nel db
-                prismaQuery =
-                {
-                    "where"     :   { "id" : { "in" : categories } },
-                    "select"    :   { "id" : true }
-                };
-                categoriesCheck = await prismaOperator(prisma, "category", "findMany", prismaQuery);
-            }
-            if (!categoriesCheck.success)
-                errorToThrow = new ErrorFromDB("Service temporarily unavailable", 503, "PICTURES (private) - UPDATE");
-            // Il seguente blocco "else if" consente di verificare quali siano le categories richieste e non esistenti (laddove ce ne siano) e, conseguentemente lanciare un errore. Avendo inizializzato a ("data" : []) l'oggetto "categoriesCheck" si ha la certezza di poter eseguire correttamente la seguente condizione sempre, senza incorrere in alcun errore logico anche nel caso in cui non siano stati richiesti collegamenti ad alcuna category.
-            else if (categoriesCheck.data.length < categories.length)
-            {
-                categories.forEach( catId =>
-                    {
-                        if (!categoriesCheck.data.some( idObject => idObject.id === catId ))
-                            missingCategories.push(catId);
-                    });
-                errorToThrow = new ErrorResourceNotFound(`Category Ids [${missingCategories}]`, "PICTURES (private) - UPDATE");
-            }
-            else
-            {
-                prismaQuery =
-                {
-                    "where"     :   {   "id"            :   id },
-                    "data"      :   {
-                                        "title"         :   title ?? pictureToUpdate.data.title,
-                                        "description"   :   description ?? pictureToUpdate.data.description,
-                                        "image"         :   pictureToUpdate.data.image,
-                                        "visible"       :   (visible !== undefined) ? visible : pictureToUpdate.data.visible,
-                                        "userId"        :   pictureToUpdate.data.userId,
-                                        "categories"    :   {
-                                                                "set"   :   categories.map( catId => ({ "id" : catId }))
-                                                            }
-                                    },
-                    "include"   :   {   
-                                        "categories"    :   {
-                                                                "select"    :   { "id" : true }
-                                                            }
-                                    }
-                };
-                const updatedPicture = await prismaOperator(prisma, "picture", "update", prismaQuery);
-                if ((!updatedPicture.success) || (!updatedPicture.data))
-                    errorToThrow = new ErrorFromDB("Operation refused", 403, "PICTURES (private) - UPDATE");
-                else 
-                {
-                    const picture = updatedPicture.data;
-                    formattedOutput("PICTURES (private) - UPDATE - SUCCESS", "***** Status: 200", "***** Previous: ", pictureToUpdate.data, "***** Updated: ", picture);
-                    return res.json({ previous : {...pictureToUpdate.data}, updated : {...picture} });
-                }
-            }
-        }
+            "where"     :   {   "id"            :   id },
+            "data"      :   {
+                                "title"         :   title ?? picture.title,
+                                "description"   :   description ?? picture.description,
+                                "image"         :   picture.image,
+                                "visible"       :   (visible !== undefined) ? visible : picture.visible,
+                                "userId"        :   picture.userId,
+                                "categories"    :   { "set"     :   connectedCategories.map( catId => ({ "id" : catId })) }
+                            },
+            "include"   :   {   "categories"    :   { "select"  :   { "id" : true } } }
+        };
+        const updatedPicture = await updateRecord("picture", prismaQuery, "PICTURES (private) - UPDATE");
+        formattedOutput("PICTURES (private) - UPDATE - SUCCESS", "***** Status: 200", "***** Previous: ", picture, "***** Updated: ", updatedPicture);
+        return res.json({ previous : {...picture}, updated : {...updatedPicture} });
     }
-    return next(errorToThrow);
+    catch(error)
+    {
+        return next(error);
+    }
 }
 
 async function destroy(req, res, next)
--- server/utilities/prismaCalls.js
+++ server/utilities/prismaCalls.js
@@ -1,4 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const noError = 0;
 const errorIfExists = 1;
@@ -10,17 +11,17 @@ const ErrorResourceNotFound = require("../exceptionsAndMiddlewares/exceptions/Er
 const ErrorUserNotAllowed = require("../exceptionsAndMiddlewares/exceptions/ErrorUserNotAllowed");
 
 /**
- * Funzione delegata ad eseguire operazioni su database.
+ * Funzione delegata ad eseguire operazioni su database, mediante Prisma.
  * @function
  * @async
  * @param {String} model - Nome del modello Prisma su cui operare
  * @param {String} operator - Nome del metodo Prisma da utilizzare (esempio: findUnique, findFirst, findMany, ...)
  * @param {Object} query - Query da passare al metodo Prisma per interrogare il database
  * @param {string} callerBlock - Stringa identificativa del blocco chiamante
- * @returns {Promise<any>} - Promise che si risolve con i dati ricevuti o generando un errore specifico */
+ * @returns {Promise<any>} - Promise che si risolve con i dati ricevuti o generando un errore specifico 
+ */
 const prismaCall = async (model, operator, query, callerBlock) => 
 {
-    const prisma = new PrismaClient();
     try
     {
         const result = await prisma[model][operator](query);
@@ -32,165 +33,183 @@ const prismaCall = async (model, operator, query, callerBlock) =>
     }
 }
 
+/**
+ * Funzione per creare un record nel database.
+ * @function
+ * @async
+ * @param {String} model - Nome del modello Prisma per cui creare un record
+ * @param {Object} query - Query da passare al metodo Prisma create
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con il record creato o generando un errore specifico
+ */
 const createRecord = async (model, query, callerBlock) =>
 {
-    try
-    {
-        const result = await prismaCall(model, "create", query, callerBlock);
-        if (!result)
-            throw new ErrorFromDB("Operation refused", 403, callerBlock);
-        else
-            return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const result = await prismaCall(model, "create", query, callerBlock);
+    if (!result)
+        throw new ErrorFromDB("Operation refused", 403, callerBlock);
+    else
+        return result;
 }
 
+/**
+ * Funzione per aggiornare un record nel database.
+ * @function
+ * @async
+ * @param {String} model - Nome del modello Prisma per cui aggiornare un record
+ * @param {Object} query - Query da passare al metodo Prisma update
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con il record aggiornato o generando un errore specifico
+ */
 const updateRecord = async (model, query, callerBlock) =>
 {
-    try
-    {
-        const result = await prismaCall(model, "update", query, callerBlock);
-        if (!result)
-            throw new ErrorFromDB("Service temporarily unavailable", 503, callerBlock);
-        else
-            return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const result = await prismaCall(model, "update", query, callerBlock);
+    if (!result)
+        throw new ErrorFromDB("Service temporarily unavailable", 503, callerBlock);
+    else
+        return result;
 }
 
+/**
+ * Funzione per eliminare un record nel database.
+ * @function
+ * @async
+ * @param {String} model - Nome del modello Prisma da cui eliminare un record
+ * @param {Object} query - Query da passare al metodo Prisma delete
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @param {string} stringForErrorMessage - Stringa aggiuntiva per il messaggio di errore
+ * @returns {Promise<any>} - Promise che si risolve con il risultato dell'eliminazione o generando un errore specifico
+ */
 const deleteRecord = async (model, query, callerBlock, stringForErrorMessage = "") =>
 {
-    try
-    {
-        await getUniqueItem(model, query, errorIfDoesntExist, callerBlock, stringForErrorMessage);
-        const result = await prismaCall(model, "delete", query, callerBlock);
-        return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    await getUniqueItem(model, query, errorIfDoesntExist, callerBlock, stringForErrorMessage);
+    const result = await prismaCall(model, "delete", query, callerBlock);
+    return result;
 }
 
+/**
+ * Funzione per ottenere un elemento unico dal database.
+ * @function
+ * @async
+ * @param {String} model - Nome del modello Prisma in cui cercare un elemento unico
+ * @param {Object} query - Query da passare al metodo Prisma findUnique
+ * @param {number} errorType - Tipo di errore da generare se l'elemento esiste o meno
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @param {string} stringForErrorMessage - Stringa aggiuntiva per il messaggio di errore
+ * @returns {Promise<any>} - Promise che si risolve con l'elemento ricevuto o generando un errore specifico
+ */
 const getUniqueItem = async (model, query, errorType, callerBlock, stringForErrorMessage) =>
 {
-    try
-    {
-        const result = await prismaCall(model, "findUnique", query, callerBlock);
-        if (result && (errorType === errorIfExists))
-            throw new ErrorRepeatedData(stringForErrorMessage, callerBlock);
-        else if (!result && (errorType === errorIfDoesntExist))
-            throw new ErrorResourceNotFound(stringForErrorMessage, callerBlock);
-        else
-            return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const result = await prismaCall(model, "findUnique", query, callerBlock);
+    if (result && (errorType === errorIfExists))
+        throw new ErrorRepeatedData(stringForErrorMessage, callerBlock);
+    else if (!result && (errorType === errorIfDoesntExist))
+        throw new ErrorResourceNotFound(stringForErrorMessage, callerBlock);
+    else
+        return result;
 }
 
+/**
+ * Funzione per ottenere un utente dal database utilizzando l'indirizzo email.
+ * @function
+ * @async
+ * @param {String} email - Indirizzo email dell'utente da cercare
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con l'utente ricevuto o generando un errore specifico
+ */
 const getUser = async (email, callerBlock) =>
 {
-    try
-    {
-        const result = await getUniqueItem("user", { "where" : { "email" : email } }, errorIfDoesntExist, callerBlock, "user");
-        return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const result = await getUniqueItem("user", { "where" : { "email" : email } }, errorIfDoesntExist, callerBlock, "user");
+    return result;
 }
 
+/**
+ * Funzione per verificare l'esistenza di un indirizzo email nel database.
+ * @function
+ * @async
+ * @param {String} email - Indirizzo email da verificare
+ * @param {number} errorType - Tipo di errore da generare se l'indirizzo email esiste o meno
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con il risultato della verifica o generando un errore specifico
+ */
 const checkEmail = async (email, errorType, callerBlock) =>
 {
-    try
-    {
-        const result = await getUniqueItem("user", { "where" : { "email" : email } }, errorType, callerBlock, "email");
-        return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const result = await getUniqueItem("user", { "where" : { "email" : email } }, errorType, callerBlock, "email");
+    return result;
 }
 
+/**
+ * Funzione per verificare l'esistenza di uno slug nel database.
+ * @function
+ * @async
+ * @param {String} slug - Slug da verificare
+ * @param {number} errorType - Tipo di errore da generare se lo slug esiste o meno
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con il risultato della verifica o generando un errore specifico
+ */
 const checkSlug = async (slug, errorType, callerBlock) =>
 {
-    try
-    {
-        const result = await getUniqueItem("category", { "where" : { "slug" : slug } }, errorType, callerBlock, "slug");
-        return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const result = await getUniqueItem("category", { "where" : { "slug" : slug } }, errorType, callerBlock, "slug");
+    return result;
 }
 
+/**
+ * Funzione per verificare la proprietà di un'immagine.
+ * @function
+ * @async
+ * @param {String} pictureId - ID dell'immagine da verificare
+ * @param {String} userId - ID dell'utente
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con l'informazione di proprietà o generando un errore specifico
+ */
 const checkPictureOwnership = async (pictureId, userId, callerBlock) =>
 {
-    try
-    {
-        const picture = await getUniqueItem("picture", { "where" : { "id" : pictureId } }, errorIfDoesntExist, callerBlock, `Picture Id [${pictureId}]`);
-        return (picture.userId == userId);
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const query = { "where" : { "id" : pictureId }, "include" : { "categories" : { "select" : { "id" : true } } } };
+    const picture = await getUniqueItem("picture", query, errorIfDoesntExist, callerBlock, `Picture Id [${pictureId}]`);
+    return { "ownership" : (picture.userId == userId), "picture" : picture };
 }
 
+/**
+ * Funzione per eliminare un'immagine se l'utente è il proprietario.
+ * @function
+ * @async
+ * @param {String} pictureId - ID dell'immagine da eliminare
+ * @param {String} userId - ID dell'utente
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con il risultato dell'eliminazione o generando un errore specifico
+ */
 const deletePictureIfOwner = async (pictureId, userId, callerBlock) =>
 {
-    try
-    {
-        const ownership = await checkPictureOwnership(pictureId, userId, callerBlock);
-        if (!ownership)
-            throw new ErrorUserNotAllowed("User not allowed to delete another user's picture", callerBlock);
-        const result = await prismaCall("picture", "delete", { "where" : { "id" : pictureId } }, callerBlock);
-        return result;
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    const { ownership } = await checkPictureOwnership(pictureId, userId, callerBlock);
+    if (!ownership)
+        throw new ErrorUserNotAllowed("User not allowed to delete another user's picture", callerBlock);
+    const result = await prismaCall("picture", "delete", { "where" : { "id" : pictureId } }, callerBlock);
+    return result;
 }
 
+/**
+ * Funzione per trovare tutte le categorie nel database in base agli ID forniti.
+ * @function
+ * @async
+ * @param {Array} categoriesToFind - Array di ID delle categorie da trovare
+ * @param {string} callerBlock - Stringa identificativa del blocco chiamante
+ * @returns {Promise<any>} - Promise che si risolve con le categorie trovate e mancanti
+ */
 const findAllCategories = async (categoriesToFind, callerBlock) =>
 {
     let found = [];
     let missing = [];
-    let query = 
+    let query = { "where" : { "id" : { "in" : categoriesToFind } }, "select"    :   { "id" : true } };
+    if (categoriesToFind.length !== 0)
     {
-        "where"     :   { "id" : { "in" : categoriesToFind } },
-        "select"    :   { "id" : true }
+        found = await prismaCall("category", "findMany", query, callerBlock);
+        if (found.length < categoriesToFind.length)
+            categoriesToFind.forEach( catId =>
+                {
+                    if (!found.some( foundId => foundId.id === catId))
+                        missing.push(catId);
+                });
     }
-    try
-    {
-        if (categoriesToFind.length !== 0)
-        {
-            found = await prismaCall("category", "findMany", query, callerBlock);
-            if (found.length < categoriesToFind.length)
-                categoriesToFind.forEach( catId =>
-                    {
-                        if (!found.some( foundId => foundId.id === catId))
-                            missing.push(catId);
-                    });
-        }
-        return { "foundCategories" : found, "missingCategories" : missing };
-    }
-    catch(error)
-    {
-        throw error;
-    }
+    return { "foundCategories" : found, "missingCategories" : missing };
 }
 
 module.exports = 
